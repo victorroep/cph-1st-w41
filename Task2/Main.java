559bea6b3b9caa4064501d6796c71f700471a3c5
vroep <victorroepstorff@gmail.com>
2021-10-05 11:22:39 +0200

    update

--- Task2/Main.java
+++ Task2/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 public class Main {
+    static ArrayList<Room> rooms;
+    static int numberOfLamps;
     public static void main(String[] args) {
         //2.G
         Room room = new Room(4, 2, 1, 2);
@@ -8,7 +10,22 @@ public class Main {
         Room room2 = new Room(4, 1, 3, 4);
 
         //2.H
-        ArrayList<Room> rooms;
-        //rooms.add(room);
+        rooms.add(room);
+        rooms.add(room1);
+        rooms.add(room2);
+
+        //2.I
+        Building building = new Building(rooms, 2, 2, true);
+
+        //2.J
+        for(Room _room : rooms){
+            numberOfLamps += _room.getNumberOfLamps();
+        }
+        System.out.println("total number of lamps "+numberOfLamps);
+
+        //2.K
+        if(){
+            
+        }
     }
 }
